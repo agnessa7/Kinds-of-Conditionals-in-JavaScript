e8ef3805d9db831329896284413f6732002c7d38
Agnessa Nikolayev <122400939+agnessa7@users.noreply.github.com>
2023-03-28 11:49:45 +0000

    Formatting

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 function isPip(slime){
   if (slime === 'pip'){
-    return 'Good morning Pip!' //Lines 1-3 Just an If statement
+    return 'Good morning Pip!' 
   } else if(typeof slime === 'string'){
-      return `Good day   
-       ${slime}!`
+      return `Good day ${slime}!`
   } else {
       return 'Sorry, something went wrong! Please enter a valid slime name.'
   }
 }
 function isDucky(bird){
   bird === 'ducky'?'Hi Ducky, you are cute!' :`Hi ${bird}!`
-}//Ternary lines 11-13
+}
 function isPoogie(pig){
   switch(pig){
     case 'poogie':
@@ -20,7 +19,12 @@ function isPoogie(pig){
     default:
       return `Hi ${pig}!`
   }
-}//Switch lines 14-23
+}
+
+
+console.log('Hello there traveler! please feel free to try out the functions in the console!!!')
 
 //in the console type the function name('') ex: isPip('pip') to get started!
-console.log('Hello there traveler! please feel free to try out the functions in the console!!!')
+//Lines 1-3 Just an If statement
+//Ternary lines 11-13
+//Switch lines 14-23
